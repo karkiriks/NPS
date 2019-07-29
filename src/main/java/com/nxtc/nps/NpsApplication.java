be3dac32b112625073a3ac64a49f0d33d28fa920
Rikesh Karkee <riky@Rikeshs-MacBook-Pro.local>
2019-07-29 15:26:14 -0500

    Init Git Project

---
+++ src/main/java/com/nxtc/nps/NpsApplication.java
@@ -0,0 +1,13 @@
+package com.nxtc.nps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsApplication.class, args);
+	}
+
+}
