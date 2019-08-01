93dfaa4201ce0552ba7adb2a418236fcb49bbbcd
Romal Basnet <romal.basnet@gmail.com>
2019-07-31 18:53:46 -0600

    controller was created

--- src/main/java/com/nxtc/nps/NpsApplication.java
+++ src/main/java/com/nxtc/nps/NpsApplication.java
@@ -2,8 +2,10 @@ package com.nxtc.nps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.nxtc.*"})
 public class NpsApplication {
 
 	public static void main(String[] args) {
